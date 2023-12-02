25b2e29d1705f1026c093c13f603fb1c21870da2
Peter Ball <114352915+peterb-1@users.noreply.github.com>
2023-12-02 13:18:53 +0000

    Day 2

--- AdventOfCode2023/Day2Solution.cs
+++ AdventOfCode2023/Day2Solution.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2023
 {
@@ -11,12 +13,36 @@ namespace AdventOfCode2023
 
         public string RunPart1(IEnumerable<string> inputLines)
         {
-            throw new System.NotImplementedException();
+            return inputLines
+                .Select(line => line.Split(',', ':', ';'))
+                .Where(line => line.All(ValidateEntry))
+                .Sum(GetLineNumber)
+                .ToString();
+            
+            bool ValidateEntry(string entry) =>
+                entry.Contains("Game") || 
+                ValidateColourEntry("red", entry, 12) || 
+                ValidateColourEntry("green", entry, 13) || 
+                ValidateColourEntry("blue", entry, 14);
+
+            bool ValidateColourEntry(string colour, string entry, int max) => entry.Contains(colour) && GetEntryCount(entry) <= max;
+
+            int GetLineNumber(string[] line) => int.Parse(line[0].Substring(5));
         }
 
         public string RunPart2(IEnumerable<string> inputLines)
         {
-            throw new System.NotImplementedException();
+            return inputLines
+                .Select(line => GetMinPossibleProduct(line.Split(',', ':', ';')))
+                .Sum().ToString();
+            
+            int GetMinPossibleProduct(string[] line) => 
+                GetMinPossibleCount("red", line) * GetMinPossibleCount("green", line) * GetMinPossibleCount("blue", line);
+
+            int GetMinPossibleCount(string colour, string[] line) => 
+                line.Aggregate(0, (accumulator, entry) => entry.Contains(colour) ? Math.Max(accumulator, GetEntryCount(entry)) : accumulator);
         }
+
+        private static int GetEntryCount(string entry) => int.Parse(entry.Substring(0, 3).Trim(' '));
     }
 }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -9,15 +9,17 @@ namespace AdventOfCode2023
         {
             var solutionsToRun = new ISolution[]
             {
-                new Day1Solution()
+                new Day1Solution(),
+                new Day2Solution()
             };
 
             foreach (var solution in solutionsToRun)
             {
                 var day = solution.GetDayNumber();
                 var inputLines = File.ReadLines($@"..\..\Input\Day{day}.txt");
-                
-                Console.Out.WriteLine($"{ solution.RunPart1(inputLines) }\n{ solution.RunPart2(inputLines) }\n");
+
+                Console.Out.WriteLine($"{ solution.RunPart1(inputLines) }");
+                Console.Out.WriteLine($"{ solution.RunPart2(inputLines) }\n");
             }
         }
     }
